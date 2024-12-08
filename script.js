9a9814165c4873ca16fe78ee5cfcea820f7654e7
DianaFarhat <dsf04@mail.aub.edu>
2024-12-09 01:40:40 +0200

    Center navbar content in nav.html for better layout alignment

--- script.js
+++ script.js
@@ -14,4 +14,16 @@ if (close) {
   });
 }
 
-
+function loadNavbar(){
+  document.addEventListener("DOMContentLoaded", function() {
+    var header = document.getElementById("header");
+    fetch("nav.html")
+    .then(response => response.text())
+    .then(data => {
+        header.innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error loading nav.html:', error);
+    });
+});
+}
